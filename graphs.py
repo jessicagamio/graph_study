c289375976c8c62574816d2e8b0e86521eba5100
jessicagamio <jessica.gamio@gmail.com>
2019-10-25 11:24:07 -0700

    completed classes

--- graphs.py
+++ graphs.py
@@ -12,4 +12,21 @@ class Graph(object):
         self.numVertices=0
 
     def addVertex(self, key):
-        
+        newVert = Vertex(key)
+        self.vertlist[key]=newVert
+        return newVert
+
+    def getVertex(self, n):
+        if n in self.vertlist:
+            return self.verlist[n]
+        else:
+            return None
+
+    def addEdge(self, f, t, weight):
+        if f not in self.vertlist:
+            self.addVertex(f)
+        if t not in self.verlist:
+            self.addVertex(t)
+        self.vertlist[f].add_Neighbor(self.verlist[t], weight)
+
+
